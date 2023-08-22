e4ec21bd106b1fe3d1725353194239e3cc9a80ab
MaelStudio <66912947+MaelStudio@users.noreply.github.com>
2023-08-22 18:35:23 -0400

    Import code

--- rocket_altimeter/rocket_altimeter.ino
+++ rocket_altimeter/rocket_altimeter.ino
@@ -2,12 +2,11 @@
 #include <Adafruit_BMP280.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-//#include <Servo.h>
+#include <Servo.h>
 
 // pins
 const byte buttonPin = 2;
 const byte servoPin = 3;
-const byte buzzerPin = 3;
 
 // BMP280 altimeter
 Adafruit_BMP280 bmp;
@@ -20,34 +19,33 @@ Adafruit_SSD1306 display(128, 64, &Wire, -1);
 const int oledAddress = 0x3C;
 
 // servo
-//Servo servo;
-//const int servoHomePos = 0;
-//const int servoTriggerPos = 120;
-
-// variables
-float height;
-float duration;
+Servo servo;
+const int servoHomePos = 0;
+const int servoTriggerPos = 180;
 
 void setup() {
+  
   // setup io
   pinMode(buttonPin, INPUT_PULLUP);
-  pinMode(buzzerPin, OUTPUT);
   bmp.begin(bmpAddress);
   display.begin(SSD1306_SWITCHCAPVCC, oledAddress);
-  //servo.attach(servoPin);
+  servo.attach(servoPin);
 
-  //servo.write(servoHomePos); // set servo to home position
+  servo.write(servoHomePos); // set servo to home position
   
   // debug menu
   bool debug = !digitalRead(buttonPin);
   if (debug) {
-    displayText("Entering\ndebug menu");
-    delay(2000);
+    unsigned long start = millis();
+    while(millis()-start < 1000) {
+      displayText("Entering\ndebug menu", false);
+      displayProgressBar(float(millis()-start)/1000, 2, 36, 124, 10);
+      display.display();
+    }
     return; // go to loop
   }
 
   // set ground altitude
-  displayText("Setting\nground\naltitude");
   float ground = 0;
   unsigned long start = millis();
   while(millis()-start < 5000) {
@@ -55,10 +53,13 @@ void setup() {
     if(altitude > ground) {
       ground = altitude;
     }
+    displayText("Setting\nground\naltitude", false);
+    displayProgressBar(float(millis()-start)/5000, 2, 52, 124, 10);
+    display.display();
     delay(measureDelay);
   }
   
-  displayText("Ready for launch!");
+  displayText("Ready for launch!", true);
   delay(2000);
   display.clearDisplay();
   display.display();
@@ -68,10 +69,9 @@ void setup() {
     
     // detect launch if rocket is 1m higher than ground
     float altitude = bmp.readAltitude(seaPressure);
-    if(altitude - ground > 1) {
+    if(altitude - ground > 2) {
       break;
     }
-    delay(measureDelay);
   }
   
   const unsigned long launchTime = millis(); // save launch time
@@ -84,7 +84,7 @@ void setup() {
     float altitude = bmp.readAltitude(seaPressure);
     if(altitude > highest) {
       highest = altitude;
-    } else if (altitude - highest < -1) {
+    } else if (altitude - highest < -2) {
       break;
     }
     delay(measureDelay);
@@ -94,26 +94,38 @@ void setup() {
   deployParachute();
 
   // flight statistics
-  height = highest - ground;
-  height = roundFloat(height, 2);
+
+  float height = highest - ground; // get height
   
-  duration = float((apogeeTime - launchTime)) / 1000;
-  duration = roundFloat(duration, 2);
+  char heightStr[6];
+  dtostrf(height, 6, 2, heightStr); // convert height to text
+  char heightDisplay[20];
+  strcpy(heightDisplay, "Altitude:\n\n");
+  strcat(heightDisplay, heightStr);
+  strcat(heightDisplay, " m");
   
+  float duration = float((apogeeTime - launchTime)) / 1000; // get duration
+
+  char durationStr[6];
+  dtostrf(duration, 6, 2, durationStr); // convert height to text // convert duration to text
+  char durationDisplay[20];
+  strcpy(durationDisplay, "Duration:\n\n");
+  strcat(durationDisplay, durationStr);
+  strcat(durationDisplay, " s");
   
-  String stat = "";
+  bool stat = 0;
   
   // show flight stats on OLED (height, duration, avg speed)
   while(true) {
     waitForButton(false);
     waitForButton(true);
-    
-    if(stat == "" || stat == "Duration") {
-      stat = "Height";
-      displayText(stat + "\n" + height + " m");
+
+    if(!stat) {
+      displayText(heightDisplay, true);
+      stat = !stat;
     } else {
-      stat = "Duration";
-      displayText(stat + "\n" + duration + " s");
+      displayText(durationDisplay, true);
+      stat = !stat;
     }
   }
 }
@@ -130,21 +142,27 @@ void loop() {
   delay(50);
 }
 
-void displayText(String text) {
+void displayText(char * text, bool send) {
   display.clearDisplay();
   display.setTextColor(SSD1306_WHITE);
   display.setCursor(0, 0);
   display.setTextSize(2);
   display.print(text);
-  display.display();
+  if(send){
+    display.display();
+  }
+}
+
+void displayProgressBar(float progress, int x, int y, int w, int h) {
+  display.drawRect(x, y, w, h, SSD1306_WHITE);
+  display.fillRect(x+3, y+3, ceil(w*progress)-6, h-6, SSD1306_WHITE);
 }
 
 void deployParachute() {
   // trigger servo
-  //servo.write(servoTriggerPos);
-  //delay(500);
-  //servo.write(servoHomePos);
-  tone(buzzerPin, 800, 500);
+  servo.write(servoTriggerPos);
+  delay(500);
+  servo.write(servoHomePos);
 }
 
 void waitForButton(bool trigger) {
